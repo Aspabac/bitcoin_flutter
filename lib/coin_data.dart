ff0082042dcfe79e1256f3a52ceb9d940fa3a2ec
Daigomizuki <daigo3262@gmail.com>
2020-11-09 19:34:43 +0900

    made a option that let user select currency

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -36,9 +36,9 @@ const apiKey = '98BBEDC5-B8D0-4864-851B-75E9F0AB0B69';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
-  Future<dynamic> getCoinData() async {
+  Future<dynamic> getCoinData(String selectedCurrency) async {
 
-    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
     http.Response response = await http.get(requestURL);
     if(response.statusCode == 200 ) {
       var decodeData =  jsonDecode(response.body);
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -29,6 +29,7 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData();
         });
       },
     );
@@ -43,7 +44,7 @@ class _PriceScreenState extends State<PriceScreen> {
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
         selectedCurrency = currenciesList[selectedIndex];
-        print(selectedIndex);
+        getData();
       },
       children: pickerItem,
     );
@@ -53,7 +54,7 @@ class _PriceScreenState extends State<PriceScreen> {
 
   void getData() async {
     try{
-      double data = await CoinData().getCoinData();
+      double data = await CoinData().getCoinData(selectedCurrency);
       setState(() {
         bitcoinValueInUSD = data.toStringAsFixed(0);
       });
@@ -89,7 +90,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = $bitcoinValueInUSD USD',
+                  '1 BTC = $bitcoinValueInUSD $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
